853dd7ff979c794930af4712a70bc01d6b3e1742
armageddon <armageddon1x@gmail.com>
2015-11-26 21:07:24 -0500

    edited code, broke sound and fixed it

--- js/function.js
+++ js/function.js
@@ -1,52 +1,37 @@
 $(function(){
+  //-----------------------------------------------------------//
 	//start
-
   //-----------------------------------------------------------//
-	//load sound
-	var beep=document.createElement("audio");
-	beep.setAttribute("src","sounds/beep.mp3");
-
-  //-----------------------------------------------------------//
-	//sample sound for buttons
-	$(".button").click(
-		function(){
-			beep.pause();
-			beep.currentTime=0;
-			beep.play();
-		}
-	);
 
   //-----------------------------------------------------------//
 	//flag for opening/closing panel
+  //-----------------------------------------------------------//
 	var flag=true;
 
 	//animation to open/close side panel
 	$("#arrow").click(function(){
-  	if(flag){
-    	$("#panel").animate({left:"70%"});
-    	flag=!flag;
-    }
-    else{
-    	$("#panel").animate({left:"97%"});
-    	flag=!flag;
-    }
+  	if(flag){$("#panel").animate({left:"70%"});}
+    else{$("#panel").animate({left:"97%"});}
+    flag=!flag;
   });
 
   //-----------------------------------------------------------//
 	//bind styled button to file upload button
-  $(".sound_button").bind("click", function(){
+  //-----------------------------------------------------------//
+  $(".sound_button").on("click", function(){
     $(this).siblings(".html_sound_button").click();
   });
-
   
   //-----------------------------------------------------------//
   //output ID3 information
-  $("sound_button").click(function(){
-    $(this).siblings(".html_sound_button").on("change",read_sound_file,false);
-  });
+  //-----------------------------------------------------------//
+  // $(".sound_button").click(function(){
+  //   $(this).siblings(".html_sound_button").on("change",handleFileSelect,false);
+  // });
 
   //-----------------------------------------------------------//
   //get the ID3 tags and output the tags
+  //-----------------------------------------------------------//
   $(".html_sound_button").change(function(e){
     //setting the file
     var file=e.currentTarget.files[0];
@@ -63,89 +48,112 @@ $(function(){
 
     //creating audio 
     objectUrl = URL.createObjectURL(file);
-    $(".audio").prop("src", objectUrl);
-
-    // console.log(new_var);
-    // console.log(objectUrl);
-
-    // var soundbite=document.createElement("audio");
-    // soundbite.setAttribute("src",objectUrl);
-
-    //  $(".button").click(
-    //   function(){
-    //     // $(".audio").pause();
-    //     $(".audio").currentTime=0;
-    //     $(".audio").play();
-    //   }
-    // );
-
-  console.log($(".audio"));
-   
+    $("audio").prop("src", objectUrl);
   });
 
   //-----------------------------------------------------------//
   //get the duration of the song
+  //-----------------------------------------------------------//
   var objectUrl;
-  $(".audio").on("canplaythrough", function(e){
-    var length=e.currentTarget.duration;
-    var duration=$(this).siblings(".file_duration");
-    
-    var minutes=Math.floor(length/60);
-    var seconds=(length%60).toFixed(0);
 
+  $(".sound_button").click(function(){
+    $(this).siblings(".player").on("canplaythrough", function(e){
+      console.log("ggggdddd");
 
-    var time=minutes+":";
-    if(seconds<10){time=time+"0"+seconds;}
-    else{time=time+seconds;}
+      var length=e.currentTarget.duration;
+      var duration=$(this).siblings(".file_duration");
+      
+      var minutes=Math.floor(length/60);
+      var seconds=(length%60).toFixed(0);
 
-    print(duration, time);
+      var time=minutes+":";
+      if(seconds<10){time=time+"0"+seconds;}
+      else{time=time+seconds;}
 
-    //  $(".button").click(
-    //   function(){
-    //     soundbite.pause();
-    //     soundbite.currentTime=0;
-    //     soundbite.play();
-    //   }
-    //  );
+      print(duration, time);
 
-    URL.revokeObjectURL(objectUrl);
-});
+      URL.revokeObjectURL(objectUrl);
+    });
+  })
 
-  function handleFileSelect(evt) {//console.log('evt', evt.target.files);
-    var files = evt.target.files; // FileList object
-    playFile(files[0]);
-  }
+  // console.log("player: "+player);
 
-  function playFile(file) {
-    console.log('file', file);
-    var freader = new FileReader();   
-    freader.onload = function(e) {
-      player.src = e.target.result;
-    };
-    freader.readAsDataURL(file);
-  }
+  // console.log("aayyy");
 
-  player = document.getElementById('player');
-  //player=$(".player");
+  //     //
+  //     console.log("this: "+$(this));
+  //     console.log($(this).attr("id"));
 
-  document.getElementById('sound_1').addEventListener('change', handleFileSelect, false);
-  //$(".files").on("change",handleFileSelect,false);
+  //     var player_id=$(this).attr("id");
 
-  document.getElementById('color_1').onclick = function(){ player.pause();
-    player.currentTime=0;player.play(); };
-  //$(".play").click(function(){
-    //player.pause();
-    //player.currentTime=0;
-    //player.play();
-  //});
+  //     // player=$(this)[0];
+  //     player=$(player_id);
+  //     console.log("player: "+player);
+  //     //
 
+  // $("#player").siblings(".html_sound_button").on("change",handleFileSelect,false);
+//gets player element
+  player=$("#player")[0];
+  console.log(player);
+
+  play_sound($("#color_1"));
+  play_sound($("#color_2"));
+  // play_sound($("#color_3"));
+  // play_sound($("#color_4"));
+  // play_sound($("#color_5"));
+  // play_sound($("#color_6"));
+  // play_sound($("#color_7"));
+  // play_sound($("#color_8"));
+  // play_sound($("#color_9"));
+  // play_sound($("#color_10"));
+  // play_sound($("#color_11"));
+  // play_sound($("#color_12"));
+  // play_sound($("#color_13"));
+  // play_sound($("#color_14"));
+  // play_sound($("#color_15"));
+  // play_sound($("#color_16"));
+;
+  //-----------------------------------------------------------//
+  //prints tags to screen
   //-----------------------------------------------------------//
   function print(element, tag){
     element.html(": "+tag);
   }
 
+  //-----------------------------------------------------------//
+  //functions for attaching sound files to buttons
+  //-----------------------------------------------------------//
 
+  //stores file in a variable
+  function handleFileSelect(evt) {
+    var files = evt.target.files;
+    playFile(files[0]);
+  }
 
-// 	//end
+  //
+  function playFile(file) {
+    var freader = new FileReader();   
+    freader.onload = function(e) {
+      player.src = e.target.result;
+      // console.log(e);
+      // console.log(e.target.result);
+      // console.log(player.src);
+    };
+    freader.readAsDataURL(file);
+  }
+
+  //-----------------------------------------------------------//
+  //plays sound on chosen element
+  //-----------------------------------------------------------//
+  function play_sound($element){
+    $element.click(function(){
+      player.pause();
+      player.currentTime=0;
+      player.play();
+    });
+  }
+
+  //-----------------------------------------------------------//
+ 	//end
+  //-----------------------------------------------------------//
 })
-//-----------------------------------------------------------//
